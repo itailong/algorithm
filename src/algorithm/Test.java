dfb946b6e6ee857b36bcbca3683de6b97dc4e992
zhangailong <it_ailong@163.com>
2021-04-01 15:54:43 +0800

    第二次提交

--- src/algorithm/Test.java
+++ src/algorithm/Test.java
@@ -1,12 +1,14 @@
 package algorithm;
 
+import static org.junit.Assert.*;
+
 public class Test 
 {
-	
-	
-	
-	
-	
+	@org.junit.Test
+	public void testName() throws Exception 
+	{
+		System.out.println("µ¥Ôª²âÊÔ");
+	}
 	
 	
 }
